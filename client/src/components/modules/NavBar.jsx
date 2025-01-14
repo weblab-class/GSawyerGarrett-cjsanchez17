d195ca80f33f8beaf61bc970b6332deee50b6250
Sawyer Garrett <sawyergarrett@dhcp-10-29-155-179.dyn.mit.edu>
2025-01-14 00:42:16 -0500

    navbar basics

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -1,3 +1,33 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// import "./NavBar.css";
+
+// /**
+//  * The navigation bar at the top of all pages. Takes no props.
+//  */
+// const NavBar = () => {
+//   return (
+//     <nav className="NavBar-container">
+//       <div className="NavBar-title u-inlineBlock">Vibe</div>
+//       <div classname="NavBar-linkContainer">
+//         <div classname="NavBar-browse">
+//           <Link to="/" className="NavBar-browse">
+//             Browse
+//           </Link>
+//         </div>
+//         <div classname="NavBar-profile">
+//           <Link to="/profile/" className="NavBar-profile">
+//             Profile
+//           </Link>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -9,12 +39,17 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <nav className="NavBar-container">
-      <div className="NavBar-title u-inlineBlock">Catbook</div>
-      <div className="NavBar-linkContainer u-inlineBlock">
-        <Link to="/" className="NavBar-link">
-          Home
+      <div className="NavBar-title">Vibe</div>
+      <div className="NavBar-linkContainer">
+        {/* Left-aligned "Browse" link */}
+        <Link to="/" className="NavBar-browse">
+          Browse
         </Link>
-        <Link to="/profile/" className="NavBar-link">
+      </div>
+
+      {/* Profile link container on the far right */}
+      <div className="NavBar-profileContainer">
+        <Link to="/profile/" className="NavBar-profile">
           Profile
         </Link>
       </div>
