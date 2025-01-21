9a9ee19c4a3a35da3d8d40007bbeacf17a3d9ee4
Sawyer Garrett <sawyergarrett@dhcp-10-29-155-179.dyn.MIT.EDU>
2025-01-21 15:48:28 -0500

    browse page

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../App"; // Assuming UserContext stores user data
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useContext(UserContext); // Assuming user object contains Google profile data
+
+  return (
+    <div className="profile-container">
+      {user && user.picture ? (
+        <img src={user.picture} alt="Profile" className="profile-image" />
+      ) : (
+        <p>Loading profile...</p>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
